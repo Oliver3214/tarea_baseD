672e8bda7aee74a9f2bc21176b5c323dcdd8fa7f
ollpes <55703720+Oliver3214@users.noreply.github.com>
2023-10-01 12:41:49 -0600

    Add files via upload

---
+++ Base de datos act-4.sql
@@ -0,0 +1,298 @@
+CREATE DATABASE  IF NOT EXISTS `proyecto_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `proyecto_db`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: proyecto_db
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clientes` (
+  `idClientes` int NOT NULL AUTO_INCREMENT,
+  `nombres` varchar(50) DEFAULT NULL,
+  `apellidos` varchar(50) DEFAULT NULL,
+  `NIT` varchar(12) DEFAULT NULL,
+  `genero` bit(1) DEFAULT NULL,
+  `telefono` varchar(25) DEFAULT NULL,
+  `correo_electronico` varchar(45) DEFAULT NULL,
+  `fechaingreso` datetime DEFAULT NULL,
+  PRIMARY KEY (`idClientes`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `compras`
+--
+
+DROP TABLE IF EXISTS `compras`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `compras` (
+  `idcompra` int NOT NULL AUTO_INCREMENT,
+  `no_orden_compra` int DEFAULT NULL,
+  `idproveedor` int DEFAULT NULL,
+  `fecha_orden` date DEFAULT NULL,
+  `fechaingreso` datetime DEFAULT NULL,
+  PRIMARY KEY (`idcompra`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `compras`
+--
+
+LOCK TABLES `compras` WRITE;
+/*!40000 ALTER TABLE `compras` DISABLE KEYS */;
+/*!40000 ALTER TABLE `compras` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `compras_detalle`
+--
+
+DROP TABLE IF EXISTS `compras_detalle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `compras_detalle` (
+  `idcompras_detalle` bigint NOT NULL AUTO_INCREMENT,
+  `idcompra` int DEFAULT NULL,
+  `idproducto` int DEFAULT NULL,
+  `cantidad` int DEFAULT NULL,
+  `precio_costo_unitario` decimal(8,0) DEFAULT NULL,
+  PRIMARY KEY (`idcompras_detalle`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `compras_detalle`
+--
+
+LOCK TABLES `compras_detalle` WRITE;
+/*!40000 ALTER TABLE `compras_detalle` DISABLE KEYS */;
+/*!40000 ALTER TABLE `compras_detalle` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `empleados`
+--
+
+DROP TABLE IF EXISTS `empleados`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `empleados` (
+  `idEmpleados` int NOT NULL AUTO_INCREMENT,
+  `nombres` varchar(60) DEFAULT NULL,
+  `apellidos` varchar(60) DEFAULT NULL,
+  `direccion` varchar(80) DEFAULT NULL,
+  `telefono` varchar(25) DEFAULT NULL,
+  `DPI` varchar(15) DEFAULT NULL,
+  `genero` bit(1) DEFAULT NULL,
+  `fecha_nacimiento` date DEFAULT NULL,
+  `idPuesto` smallint DEFAULT NULL,
+  `fecha_inicio_labores` date DEFAULT NULL,
+  `fechaingreso` datetime DEFAULT NULL,
+  PRIMARY KEY (`idEmpleados`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `empleados`
+--
+
+LOCK TABLES `empleados` WRITE;
+/*!40000 ALTER TABLE `empleados` DISABLE KEYS */;
+/*!40000 ALTER TABLE `empleados` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `marcas`
+--
+
+DROP TABLE IF EXISTS `marcas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `marcas` (
+  `idMarca` smallint NOT NULL AUTO_INCREMENT,
+  `marca` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`idMarca`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `marcas`
+--
+
+LOCK TABLES `marcas` WRITE;
+/*!40000 ALTER TABLE `marcas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `marcas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productos`
+--
+
+DROP TABLE IF EXISTS `productos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productos` (
+  `idProductos` int NOT NULL AUTO_INCREMENT,
+  `producto` varchar(50) DEFAULT NULL,
+  `idMarca` smallint DEFAULT NULL,
+  `Descripcion` varchar(100) DEFAULT NULL,
+  `Imagen` varchar(30) DEFAULT NULL,
+  `precio_costo` decimal(8,0) DEFAULT NULL,
+  `precio_venta` decimal(8,0) DEFAULT NULL,
+  `existencia` int DEFAULT NULL,
+  `fecha_ingreso` datetime DEFAULT NULL,
+  PRIMARY KEY (`idProductos`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productos`
+--
+
+LOCK TABLES `productos` WRITE;
+/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `proveedores`
+--
+
+DROP TABLE IF EXISTS `proveedores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `proveedores` (
+  `idProveedore` int NOT NULL AUTO_INCREMENT,
+  `proveedor` varchar(60) DEFAULT NULL,
+  `nit` varchar(12) DEFAULT NULL,
+  `direccion` varchar(80) DEFAULT NULL,
+  `telefono` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`idProveedore`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `proveedores`
+--
+
+LOCK TABLES `proveedores` WRITE;
+/*!40000 ALTER TABLE `proveedores` DISABLE KEYS */;
+/*!40000 ALTER TABLE `proveedores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `puestos`
+--
+
+DROP TABLE IF EXISTS `puestos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `puestos` (
+  `idPuestos` smallint NOT NULL AUTO_INCREMENT,
+  `puestos` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`idPuestos`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `puestos`
+--
+
+LOCK TABLES `puestos` WRITE;
+/*!40000 ALTER TABLE `puestos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `puestos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ventas`
+--
+
+DROP TABLE IF EXISTS `ventas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ventas` (
+  `idVentas` int NOT NULL AUTO_INCREMENT,
+  `nofactura` int DEFAULT NULL,
+  `serie` char(1) DEFAULT NULL,
+  `fechafactura` date DEFAULT NULL,
+  `idcliente` int DEFAULT NULL,
+  `idempleado` int DEFAULT NULL,
+  `fechaingreso` datetime DEFAULT NULL,
+  PRIMARY KEY (`idVentas`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ventas`
+--
+
+LOCK TABLES `ventas` WRITE;
+/*!40000 ALTER TABLE `ventas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ventas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ventas_detalle`
+--
+
+DROP TABLE IF EXISTS `ventas_detalle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ventas_detalle` (
+  `idventas_detalle` bigint NOT NULL AUTO_INCREMENT,
+  `id_Venta` int DEFAULT NULL,
+  `idProducto` int DEFAULT NULL,
+  `cantidad` varchar(45) DEFAULT NULL,
+  `precio_unitario` decimal(8,0) DEFAULT NULL,
+  PRIMARY KEY (`idventas_detalle`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ventas_detalle`
+--
+
+LOCK TABLES `ventas_detalle` WRITE;
+/*!40000 ALTER TABLE `ventas_detalle` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ventas_detalle` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-10-01 12:38:46
